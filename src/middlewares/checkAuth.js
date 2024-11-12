7663f48f4d6076e58586a76e4468cb09a721b48e
muntashir-wahid <muntashir.wahid@yahoo.com>
2024-11-12 16:44:33 +0600

    handled loign errors with proper message and status code

--- src/middlewares/checkAuth.js
+++ src/middlewares/checkAuth.js
@@ -19,7 +19,7 @@ exports.protect = catchAsync(async (req, _, next) => {
 
   if (token === "undefined") {
     return next(
-      new Error("You are not logged in. Please login to get the access")
+      new AppError("You are not logged in. Please login to get the access", 401)
     );
   }
 
@@ -51,7 +51,7 @@ exports.restrictTo = (...roles) => {
   return (req, _, next) => {
     if (!roles.includes(req.user.userRole)) {
       return next(
-        new Error("You do not have permission to perform this action", 403)
+        new AppError("You do not have permission to perform this action", 403)
       );
     }
 
